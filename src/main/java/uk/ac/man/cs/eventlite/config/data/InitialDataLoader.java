fcd19ee4c7d5ada4d671f02b8ad008b839125c57
Misha <michael.krasa@student.manchester.ac.uk>
2020-02-19 12:34:36 +0000

    Venue initialised in Initial data loader

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -15,6 +15,7 @@ import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 
 import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Component
 @Profile({ "default", "test" })
@@ -35,21 +36,24 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 			log.info("Database already populated. Skipping data initialization.");
 			return;
 		}else if (eventService.count() == 0) {
+			Venue testVenue = new Venue();
 			Event testEvent = new Event();
+			testVenue.setName("Kilburn 1.8");
+			testVenue.setId(1);
+			testVenue.setCapacity(1000);			
+			
 			testEvent.setId(1);
 			testEvent.setName("Test Event");
 			testEvent.setDate(LocalDate.now());
 			testEvent.setTime(LocalTime.now());
 			testEvent.setVenue(1);
 			
-			// This line causes error
+			venueService.save(testVenue);
 			eventService.save(testEvent);
-			
+						
 			log.info("Added event (" + testEvent.getId() + ") to the database.");
 			return;
 		}
-
 		// Build and save initial models here.
-
 	}
 }
