311c3e8ea6aeb9136284b05e35185bea9b3951ff
Will Cowan <william.cowan@student.manchester.ac.uk>
2020-02-26 12:06:59 +0000

    Update controller adding routes for event creation

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,14 +1,21 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 
 @Controller
 @RequestMapping(value = "/events", produces = { MediaType.TEXT_HTML_VALUE })
@@ -23,6 +30,38 @@ public class EventsController {
 		model.addAttribute("events", eventService.findAll());
 				
 		return "events/index";
+	}
+	
+	
+	/////////////////////////// ADD EVENT ////////////////////////////////////////////////////
+	
+	// GET request made by button, taking user to page "event/new" to input event details
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public String newEvent(Model model) {
+		if (!model.containsAttribute("event")) {
+			model.addAttribute("event", new Event());
+		}
+
+		return "event/new";
+	}
+	
+	// POST request made when submitting form on "event/new", to create the new event
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String createEvent(@RequestBody @Valid @ModelAttribute Event event,
+			BindingResult errors, Model model, RedirectAttributes redirectAttrs) {
+		
+		// If form has errors, stay on event/new (stay on form)
+		if (errors.hasErrors()) {
+			model.addAttribute("event", event);
+			return "event/new";
+		}
+		
+		// If no errors, save the event
+		eventService.save(event);
+		redirectAttrs.addFlashAttribute("ok_message", "New event added.");
+		
+		// Go back to /event
+		return "redirect:/event";
 	}
 
 }
