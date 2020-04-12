a219360c1929579071e9527dc5ecb3f6ec60dcb5
Alex Woods <alex.woods@student.manchester.ac.uk>
2020-04-12 18:02:41 +0100

    Added tests for the new Venues Controller findAll and findById, same as has been done for events controller

---
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -0,0 +1,160 @@
+package uk.ac.man.cs.eventlite.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.Optional;
+
+import javax.servlet.Filter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.mockito.verification.VerificationMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import uk.ac.man.cs.eventlite.config.Security;
+import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.dao.EventService;
+import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = EventLite.class)
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class VenuesControllerTest {
+	
+	private final static String BAD_ROLE = "USER";
+
+	private MockMvc mvc;
+
+	@Autowired
+	private Filter springSecurityFilterChain;
+
+	@Mock
+	private Event event;
+
+	@Mock
+	private Venue venue;
+
+	@Mock
+	private EventService eventService;
+
+	@Mock
+	private VenueService venueService;
+
+	@InjectMocks
+	private VenuesController venuesController;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mvc = MockMvcBuilders.standaloneSetup(venuesController).apply(springSecurity(springSecurityFilterChain))
+				.build();
+	}
+
+	@Test
+	public void getIndexWhenNoVenues() throws Exception {
+		when(eventService.findAll()).thenReturn(Collections.<Event> emptyList());
+		when(venueService.findAll()).thenReturn(Collections.<Venue> emptyList());
+
+		mvc.perform(get("/venues").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(handler().methodName("getAllVenues"));
+
+		verify(venueService).findAll();
+		verifyZeroInteractions(event);
+		verifyZeroInteractions(venue);
+	}
+
+	@Test
+	public void getIndexWithVenues() throws Exception {
+		
+		when(venueService.findAll()).thenReturn(Collections.<Venue> singletonList(venue));
+
+		mvc.perform(get("/venues").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(handler().methodName("getAllVenues"));
+
+		verify(venueService).findAll();
+		verifyZeroInteractions(event);
+		verifyZeroInteractions(venue);
+	}
+			
+	@Test
+	public void getVenueFromID() throws Exception {
+		
+		venue.setId(100L);
+		
+		// optional event
+		Optional<Venue> optV = Optional.of(venue);
+		
+		// mock method
+		when(venueService.findById(100L)).thenReturn(optV);
+				
+		
+		// call the correct mvc function
+		mvc.perform(get("/venues/100").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+		.andExpect(view().name("venues/view")).andExpect(handler().methodName("getVenueById"));
+		
+		
+		// check that method is called correctly, 2 times. one for assertequals, one for mvc.perform
+		verify(venueService).findById(100L);
+	}
+	
+	@Test
+	public void getNullEventFromID() throws Exception {
+		when(venueService.findById(0L)).thenReturn(Optional.empty());
+		
+		// routes to index as event 0 doesnt exist
+		mvc.perform(get("/venues/0").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("venues/index")).andExpect(handler().methodName("getVenueById"));
+		
+		verify(venueService).findById(0L);
+	}
+	
+	
+}
