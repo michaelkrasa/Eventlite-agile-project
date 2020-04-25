72266abec58bf9f44eb24937e634178a9efe03b2
Alex Woods <alex.woods@student.manchester.ac.uk>
2020-04-25 18:01:34 +0100

    Added latitude and longitude fields and a location setter
    The set location calls the mapbox geocode api to get a Point object for
    the location. From this it then gets the latitude and longitude to save
    in the lat and long fields.

--- src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
@@ -1,5 +1,9 @@
 package uk.ac.man.cs.eventlite.entities;
 
+
+import java.util.List;
+import java.util.function.BooleanSupplier;
+
 //import java.util.Set;
 
 import javax.persistence.Entity;
@@ -8,9 +12,25 @@ import javax.persistence.Id;
 //import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
+import com.mapbox.geojson.Point;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import uk.ac.man.cs.eventlite.config.data.InitialDataLoader;
+
+
 @Entity
 @Table(name = "venues")
 public class Venue {
+	
+	private final static Logger log = LoggerFactory.getLogger(Venue.class);
 	
 	@Id
 	@GeneratedValue
@@ -19,6 +39,10 @@ public class Venue {
 	private String name;
 
 	private int capacity;
+	
+	private double latitude;
+	
+	private double longitude;
 	
 //	@OneToMany(mappedBy = "venues")
 //	private Set<Event> events;
@@ -49,4 +73,52 @@ public class Venue {
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
+	
+	public void setLocation(String locationString) {
+		MapboxGeocoding locationQuery = MapboxGeocoding.builder()
+			.accessToken("pk.eyJ1IjoiajMzMDM2YWoiLCJhIjoiY2s4eGhsMjdoMDk0dzNscDVidXd0OGc3dyJ9.wDg8cWDqltVSBrXxf8R9mg")
+			.query(locationString)
+			.build();
+		
+		locationQuery.enqueueCall(new Callback<GeocodingResponse>() {
+			@Override
+			public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+		 
+				List<CarmenFeature> results = response.body().features();
+		 
+				if (results.size() > 0) {
+		 
+				  // Log the first results Point.
+				  Point location = results.get(0).center();
+				  log.info("[Geocode request] : onResponse: " + location.toString());
+				  
+				  latitude = location.latitude();
+				  longitude = location.longitude();
+				  
+				  log.info("[Geocode request] : lat,long : " + latitude + ", " + longitude);
+		 
+				} else {
+		 
+				  // No result for your request were found.
+				  log.info("[Geocode request] : onResponse: No result found");
+		 
+				}
+			}
+		 
+			@Override
+			public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+				throwable.printStackTrace();
+			}
+		});
+
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
 }
